a3f58d7789cf072fbf38a71f570dcfe141478646
Mark Nuzzolilo <nuzz604@gmail.com>
2026-09-05 20:15:30 -0700

    Update file line endings to LF

--- Polyseme.Sandbox/Program.cs
+++ Polyseme.Sandbox/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Polyseme.Core;
-using VDS.RDF;
 
 var core = CoreGraph.Build();
 core.PrintTriples(core.RdfsInferredTriples, "Schema Inferred Triples");
 core.PrintTriples(core.RuleInferredTriples, "Rule Inferred Triples");
-
+
